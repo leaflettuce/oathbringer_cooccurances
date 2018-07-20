be1e6ff07127598e37cff9bfb17739e793d70ac4
Andrew Trick <andyjtrick@gmail.com>
2018-07-20 12:55:23 -0500

    matrix made, network graph lol bad.

--- parse.py
+++ parse.py
@@ -6,7 +6,8 @@ Created on Thu Jul 19 10:33:20 2018
 """
 import re
 import pandas as pd
-
+import networkx as nx
+import matplotlib.pyplot as plt
 
 #import
 with open('data/oathbringer.txt') as text:
@@ -23,30 +24,31 @@ for section in sections:
     quotes = re.findall("“.*?”", section)
     for quote in quotes:
         section = section.replace(quote, " ")
-    cleaned_sections.append(section.lower())
+    cleaned_sections.append(section)
 
 
 #Create characters list and cooccurnce matrix
-characters = ['syl', 'pattern', 'wyndle', 'glys', 'ivory', 'timbre', 'stormfather',
-              'nightwatcher', 'dalinar', 'shallan', 'kaladin', 'venli', 'adolin', 
-              'szeth', 'navani', 'moash', 'jasnah', 'teft', 'renarin', 'lift', 
-              'taravangian', 'wit', 'eshonai', 'rock', 'lopen', 'rysn', 'sigzil',
-              'palona', 'mem', 'ellista', 'kaza', 'gawx', 'sheler', 'rlain',
-              'torol', 'meridas', 'teleb', 'gavilar', 'resi', 'elit', 'erraniv',
-              'helaran', 'jakamav', 'kalishor', 'salinor', 'tanalor', 'tinalar',
-              'teft', 'skar', 'bisig', 'dabbid', 'hobber', 'shen', 'leyten', 
-              'drehy', 'gadol', 'natam', 'peet', 'torfin', 'yake', 'baxil', 
-              'roshone', 'tavinar', 'istow', 'dukar', 'gavinor', 'gaz', 'ghenna', 
-              'hoid', 'inadara', 'isasik', 'ishikk', 'jenet', 'kadash', 'kalami', 
-              'khal', 'khriss', 'laral', 'lhan', 'lift', 'lirin', 'maben', 
-              'maib', 'marri', 'mik', 'nale', 'nazh', 'nergaoul', 'nbissiquan', 
-              'nlent', 'noura', 'odium', 'redin', 'rez', 'rial', 'rushu',
-              'sebarial', 'shalash', 'sidin', 'sja-anat', 'tag', 'taka',
-              'talik', 'temoo', 'thresh', 'tigzikk', 'toravi', 'vamah', 
-              'vao', 'vath ', 'vathah', 'veil', 'venli', 'elhokar', 'evi ', 
+characters = ['Syl ', 'Pattern ', 'Wyndle ', 'Glys ', 'Ivory ', 'Timbre ', 'Stormfather ',
+              'Nightwatcher ', 'Dalinar ', 'Shallan ', 'Kaladin ', 'Venli ', 'Adolin ', 
+              'Szeth ', 'Navani ', 'Moash ', 'Jasnah ', 'Teft ', 'Renarin ', 'Lift ', 
+              'Taravangian ', 'Wit ', 'Eshonai ', 'Rock ', 'Lopen ', 'Rysn ', 'Sigzil ',
+              'palona ', 'mem ', 'ellista ', 'kaza ', 'gawx ', 'sheler ', 'rlain ',
+              'torol ', 'meridas ', 'teleb ', 'gavilar ', 'resi ', 'elit ', 'erraniv ',
+              'helaran ', 'jakamav ', 'kalishor ', 'salinor ', 'tanalor ', 'tinalar ',
+              'skar ', 'bisig ', 'dabbid ', 'hobber ', 'shen ', 'leyten ', 
+              'drehy ', 'gadol ', 'natam ', 'peet ', 'torfin ', 'yake ', 'baxil ', 
+              'roshone ', 'tavinar ', 'istow ', 'dukar ', 'gavinor ', 'gaz ', 'ghenna ', 
+              'hoid ', 'inadara', 'isasik', 'ishikk', 'jenet', 'kadash', 'kalami', 
+              'khal ', 'khriss', 'laral ', 'lhan ', 'lirin', 'maben', 
+              'maib ', 'marri ', 'mik ', 'nale ', 'nazh', 'nergaoul', 'nbissiquan', 
+              'nlent', 'noura', 'odium', 'redin', 'rez ', 'rial', 'rushu',
+              'sebarial', 'shalash', 'sidin', 'sja-anat', 'tag ', 'taka ',
+              'talik', 'temoo', 'thresh ', 'tigzikk', 'toravi', 'vamah', 
+              'vao', 'vath ', 'vathah', 'veil ', 'elhokar', 'evi ', 
               'evinor']
+characters = [character.title() for character in characters] #oops title case
 
-#--> iterate through each
+#--> iterate through each and store in dictionary
 sections_dictionary = {}
 iterative = 0
 for section in cleaned_sections:
@@ -56,9 +58,34 @@ for section in cleaned_sections:
             if str(iterative) in sections_dictionary.keys():
                 sections_dictionary[str(iterative)].append(char)  
             else:
-                sections_dictionary[str(iterative)] = [char]
+                sections_dictionary[str(iterative)] = [char]                      
             
-    #count name of characters (only once per)
-sections_df = pd.DataFrame(sections_dictionary.items()) 
+##set base df (co-occurance matrix)
+df = pd.DataFrame(columns = characters, index = characters)
+df[:] = int(0)
 
-    #update matrix
+#iterate through each POV of book and add one for each character-character relationship
+#-> in this case, relationship equates to appearing in the same POV
+for value in sections_dictionary.values():
+    for character1 in characters:
+        for character2 in characters:
+            if character1 in value and character2 in value:
+                df[character1][character2] += 1
+                df[character2][character1] += 1
+                
+#add weights to edges
+edge_list = [] #test networkx
+for index, row in df.iterrows():
+    i = 0
+    for col in row:
+        weight = float(col)/464
+        edge_list.append((index, df.columns[i], weight))
+        i += 1
+        
+#networkx graph time!
+G = nx.Graph()
+G.add_nodes_from(characters)
+G.add_weighted_edges_from(edge_list)
+
+#draw the graph
+nx.draw(G)
